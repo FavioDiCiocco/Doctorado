8926928c7237c5285ca902828c5c7e1591d86b9f
Favio Di Ciocco <42681829+Myullnir@users.noreply.github.com>
2022-12-29 17:26:09 -0300

    Cambios_parametros_2D 29/12
    Cambié el código para empezar a estudiar datos con el modelo de Cambios_parametros en el caso de utilizar 2 tópicos.

--- Primer año/Programas C/Programas/Cambios_parametros/1D/main.c
+++ Primer año/Programas C/Programas/Cambios_parametros/1D/main.c
@@ -34,16 +34,16 @@ int main(int argc, char *argv[]){
 	// Defino los parámetros de mi modelo. Esto va desde número de agentes hasta el paso temporal de integración.
 	// Primero defino los parámetros que requieren un input.
 	ps_datos->i_N = strtol(argv[1],NULL,10); // Cantidad de agentes en el modelo
-	ps_datos->d_epsilon = strtof(argv[2],NULL); // Este es el umbral que determina si el interés del vecino puede generarme más interés.
-	ps_datos->d_Cosangulo = strtof(argv[3],NULL); // Este es el coseno de Delta que define la relación entre tópicos.
-	int i_iteracion = strtol(argv[4],NULL,10); // Número de instancia de la simulación.
+	ps_datos->d_alfa = strtof(argv[2],NULL); // Controversialidad de los tópicos
+	ps_datos->d_epsilon = strtof(argv[3],NULL); // Este es el umbral que determina si el interés del vecino puede generarme más interés.
+	ps_datos->d_kappa = strtof(argv[4],NULL); // Esta amplitud regula la relación entre el término lineal y el término logístico
+	int i_iteracion = strtol(argv[5],NULL,10); // Número de instancia de la simulación.
 	
 	// Los siguientes son los parámetros que están dados en los structs
-	ps_datos->i_T = 2;  //strtol(argv[1],NULL,10); Antes de hacer esto, arranquemos con número fijo   // Cantidad de temas sobre los que opinar
+	ps_datos->i_T = 1;  //strtol(argv[1],NULL,10); Antes de hacer esto, arranquemos con número fijo   // Cantidad de temas sobre los que opinar
 	ps_datos->i_Iteraciones_extras = 40; // Este valor es la cantidad de iteraciones extra que el sistema tiene que hacer para cersiorarse que el estado alcanzado efectivamente es estable
 	ps_datos->i_pasosprevios = 20; // Elegimos 20 de manera arbitraria con Pablo y Sebas. Sería la cantidad de pasos hacia atrás que miro para comparar cuanto varió el sistema
-	ps_datos->d_kappa = 1; // Esta amplitud regula la relación entre el término lineal y el término logístico
-	ps_datos->d_alfa = 4; // Ex-Controversialidad de los tópicos
+	ps_datos->d_Cosangulo = 0; // Este es el coseno de Delta que define la relación entre tópicos.
 	ps_datos->d_dt = 0.01; // Paso temporal de iteración del sistema
 	ps_datos->d_NormDif = sqrt(ps_datos->i_N*ps_datos->i_T); // Este es el valor de Normalización de la variación del sistema, que me da la variación promedio de las opiniones.
 	ps_datos->d_CritCorte = pow(10,-4); // Este valor es el criterio de corte. Con este criterio, toda variación más allá de la quinta cifra decimal es despreciable.
@@ -117,14 +117,14 @@ int main(int argc, char *argv[]){
 	
 	// Este archivo es el que guarda la Varprom del sistema mientras evoluciona
 	char s_Opiniones[355];
-	sprintf(s_Opiniones,"../Programas Python/Cambios_parametros/2D/Opiniones_N=%d_Cosd=%.1f_epsilon=%.2f_Iter=%d.file"
-		,ps_datos->i_N,ps_datos->d_Cosangulo,ps_datos->d_epsilon,i_iteracion);
+	sprintf(s_Opiniones,"../Programas Python/Cambios_parametros/Alfa=%d/Opiniones_N=%d_kappa=%.2f_epsilon=%.2f_Iter=%d.file"
+		,(int) ps_datos->d_alfa,ps_datos->i_N,ps_datos->d_kappa,ps_datos->d_epsilon,i_iteracion);
 	FILE *pa_Opiniones=fopen(s_Opiniones,"w"); // Con esto abro mi archivo y dirijo el puntero a él.
 	
 	// Este archivo es el que guarda las opiniones de todos los agentes del sistema.
 	char s_Testigos[355];
-	sprintf(s_Testigos,"../Programas Python/Cambios_parametros/2D/Testigos_N=%d_Cosd=%.1f_epsilon=%.2f_Iter=%d.file"
-		,ps_datos->i_N,ps_datos->d_Cosangulo,ps_datos->d_epsilon,i_iteracion);
+	sprintf(s_Testigos,"../Programas Python/Cambios_parametros/Alfa=%d/Testigos_N=%d_kappa=%.2f_epsilon=%.2f_Iter=%d.file"
+		,(int) ps_datos->d_alfa,ps_datos->i_N,ps_datos->d_kappa,ps_datos->d_epsilon,i_iteracion);
 	FILE *pa_Testigos=fopen(s_Testigos,"w"); // Con esto abro mi archivo y dirijo el puntero a él.
 	
 	// Este archivo es el que guarda las opiniones de todos los agentes del sistema.
@@ -135,10 +135,10 @@ int main(int argc, char *argv[]){
 	
 	
 	// Este archivo es el que levanta los datos de la matriz de Adyacencia de las redes generadas con Python
-	char s_matriz_adyacencia[355];
-	sprintf(s_matriz_adyacencia,"MARE/Random_Regulars/Random-regular_N=%d_ID=%d.file"
-		,ps_datos->i_N,(int) i_iteracion%100); // El 100 es porque tengo 100 redes creadas. Eso lo tengo que revisar si cambio el código
-	FILE *pa_matriz_adyacencia=fopen(s_matriz_adyacencia,"r");
+	// char s_matriz_adyacencia[355];
+	// sprintf(s_matriz_adyacencia,"MARE/Random_Regulars/Random-regular_N=%d_ID=%d.file"
+		// ,ps_datos->i_N,(int) i_iteracion%100); // El 100 es porque tengo 100 redes creadas. Eso lo tengo que revisar si cambio el código
+	// FILE *pa_matriz_adyacencia=fopen(s_matriz_adyacencia,"r");
 	
 	// Puntero a la función que define mi ecuación diferencial
 	// double (*pf_Din_Sat)(ps_Red var, ps_Param par) = &Din_saturacion;
@@ -150,10 +150,10 @@ int main(int argc, char *argv[]){
 	
 	GenerarOpi(ps_red); // Esto me inicializa mi matriz de opiniones 
 	GenerarAng(ps_red, ps_datos); // Esto me inicializa mi matriz de superposición, definiendo el solapamiento entre tópicos.
-	// GenerarAdy_Conectada(ps_red, ps_datos); // Esto me produce una matriz de adyacencia completamente conectada
+	GenerarAdy_Conectada(ps_red, ps_datos); // Esto me produce una matriz de adyacencia completamente conectada
 	
-	Lectura_Adyacencia(ps_red->pi_Adyacencia, pa_matriz_adyacencia); // Leo el archivo de la red estática y lo traslado a la matriz de adyacencia
-	fclose(pa_matriz_adyacencia); // Aprovecho y cierro el puntero al archivo de la matriz de adyacencia
+	// Lectura_Adyacencia(ps_red->pi_Adyacencia, pa_matriz_adyacencia); // Leo el archivo de la red estática y lo traslado a la matriz de adyacencia
+	// fclose(pa_matriz_adyacencia); // Aprovecho y cierro el puntero al archivo de la matriz de adyacencia
 	
 	
 	//################################################################################################################################
@@ -246,7 +246,7 @@ int main(int argc, char *argv[]){
 	fprintf(pa_Opiniones,"\n");
 	fprintf(pa_Opiniones,"Opiniones finales\n");
 	Escribir_d(ps_red->pd_Opiniones,pa_Opiniones);
-	fprintf(pa_Opiniones,"Matriz de Adyacencia\n"); // Guardo esto para poder comprobar que la red sea conexa.
+	fprintf(pa_Opiniones,"matriz de Adyacencia\n"); // Guardo esto para poder comprobar que la red sea conexa.
 	Escribir_i(ps_red->pi_Adyacencia,pa_Opiniones);
 	fprintf(pa_Opiniones,"Semilla\n");
 	fprintf(pa_Opiniones,"%ld\n",semilla);
@@ -267,7 +267,7 @@ int main(int argc, char *argv[]){
 	// Finalmente imprimo el tiempo que tarde en ejecutar todo el programa
 	time(&tt_fin);
 	f_tardanza = tt_fin-tt_prin;
-	// sleep(1);
+	sleep(1);
 	printf("Tarde %.1f segundos \n",f_tardanza);
 	
 	return 0;
