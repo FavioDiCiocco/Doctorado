f8615ebe643588e9e4367caf88028e21cbffa146
Favio Di Ciocco <42681829+Myullnir@users.noreply.github.com>
2023-11-15 12:42:45 -0300

    15/11 Mediodía

---
+++ Segundo Año/Programas Python/Prueba_metrica/Python/Generacion_datos.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 11:32:31 2023
+
+@author: favio
+"""
+
+# Voy a armar los datos que me generan mis diversas configuraciones. Esas
+# configuraciones luego las voy a usar para probar las métricas de medir opiniones.
+
